c87e24089890a90ed2b49ed5e2c26a993aa668dc
Satish <satish@example.com>
2025-05-15 10:07:58 +0000

    main.tf updated

--- main.tf
+++ main.tf
@@ -14,28 +14,3 @@ resource "aws_instance" "web" {
   }
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "flask-app-sg_1"
-  description = "Allow SSH and HTTP"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
